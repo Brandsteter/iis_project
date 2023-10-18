38cac571010410f3175ae72f7342f69e32ff06a7
LuciSirr <67655837+LuciSirr@users.noreply.github.com>
2023-10-18 22:38:37 +0200

    feat: introduced basic register ui

--- resources/js/components/auth/LoginComponent.vue
+++ resources/js/components/auth/LoginComponent.vue
@@ -3,7 +3,7 @@
     <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh; ">
             <div class="card" style="width: 400px;  background-color: lightskyblue; padding: 20px; border-radius: 10px;">
                 <form>
-                    <label style="font-size: x-large" for="Login" class="form-label">Login</label>
+                    <label style="font-size: x-large" class="form-label">Login</label>
                     <div class="mb-3">
                         <label for="InputEmail" class="form-label">Email address</label>
                         <input id="InputEmail" class="form-control" v-model="fields.email" type="email" aria-describedby="emailHelp">
--- resources/js/components/auth/RegisterComponent.vue
+++ resources/js/components/auth/RegisterComponent.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>
-        <input v-model="fields.name" type="text">
-        <input v-model="fields.email" type="email">
-        <input v-model="fields.password" type="password">
-        <input v-model="fields.passwordRepeat" type="password">
-        <button @click="submit">Submit</button>
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh; ">
+        <div class="card" style="width: 400px;  background-color: lightskyblue; padding: 20px; border-radius: 10px;">
+            <form>
+                <label style="font-size: x-large" class="form-label">Sign up</label>
+                <div class="mb-3">
+                    <label for="InputUsername" class="form-label">Username</label>
+                    <input id="InputUsername" class="form-control" v-model="fields.name" type="text" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="InputEmail" class="form-label">Email address</label>
+                    <input id="InputEmail" class="form-control" v-model="fields.email" type="email" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="InputPassword" class="form-label">Password</label>
+                    <input class="form-control" id="InputPassword" v-model="fields.password" type="password">
+                </div>
+                <div class="mb-3">
+                    <label for="InputPasswordRepeat" class="form-label">Repeat password</label>
+                    <input class="form-control" id="InputPasswordRepeat" v-model="fields.passwordRepeat" type="password">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="btn btn-primary" @click="submit">Submit</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
