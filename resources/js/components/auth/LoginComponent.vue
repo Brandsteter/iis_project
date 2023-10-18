79a5a8863c3d034cd4507fdcee93532ea3827e87
LuciSirr <67655837+LuciSirr@users.noreply.github.com>
2023-10-18 22:28:44 +0200

    feat: introduced basic login ui

--- resources/js/components/auth/LoginComponent.vue
+++ resources/js/components/auth/LoginComponent.vue
@@ -1,10 +1,25 @@
+
 <template>
-    <div>
-        <input v-model="fields.email" type="email">
-        <input v-model="fields.password" type="password">
-        <button @click="submit">Submit</button>
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh; ">
+            <div class="card" style="width: 400px;  background-color: lightskyblue; padding: 20px; border-radius: 10px;">
+                <form>
+                    <label style="font-size: x-large" for="Login" class="form-label">Login</label>
+                    <div class="mb-3">
+                        <label for="InputEmail" class="form-label">Email address</label>
+                        <input id="InputEmail" class="form-control" v-model="fields.email" type="email" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="InputPassword" class="form-label">Password</label>
+                        <input class="form-control" id="InputPassword" v-model="fields.password" type="password">
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-primary" @click="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
     </div>
 </template>
+
 
 <script>
 export default {
