3db3c14f487fef062bf0dbc166485768c2a17642
David Brandsteter <xbrand13@vutbr.cz>
2023-11-14 21:45:08 +0100

    feat: Basic Event model, migration and Controller, relation with Place

---
+++ app/Models/Event.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'is_approved',
+        'event_start',
+        'event_end',
+        'place_id',
+        'description',
+        'capacity_max',
+        'capacity_current'
+    ];
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+
+}
