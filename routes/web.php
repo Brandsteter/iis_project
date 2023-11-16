9630c20e94e8899a171efe7edc7ebce9c0285e57
David Brandsteter <xbrand13@vutbr.cz>
2023-11-16 20:12:52 +0100

    feat: getPlaces

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,7 @@ Route::prefix('/place')->group(function () {
     Route::middleware('auth')->group(function () {
         Route::post('/', [\App\Http\Controllers\PlaceController::class, 'create']);
         Route::get('/', [\App\Http\Controllers\UserController::class, 'placesPage']);
+        Route::get('/places', [\App\Http\Controllers\PlaceController::class, 'getPlaces']);
 
         Route::middleware('moderator')->group(function () {
             Route::delete('/{place}', [\App\Http\Controllers\PlaceController::class, 'delete']);
